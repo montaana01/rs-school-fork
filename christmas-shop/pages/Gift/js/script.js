05913596cdbb0abafe78a3e0d07fded083803f4f
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2024-12-03 04:31:47 +0300

    feat: add scroll to top on gift page

--- christmas-shop/pages/Gift/js/script.js
+++ christmas-shop/pages/Gift/js/script.js
@@ -1,9 +1,30 @@
-const HAMBURGER = document.getElementById('hamburger');
-const NAVIGATION = document.getElementById('navigation');
-const BODY = document.body;
+document.addEventListener('DOMContentLoaded', () => {
+    const HAMBURGER = document.getElementById('hamburger');
+    const NAVIGATION = document.getElementById('navigation');
+    const BODY = document.body;
 
-HAMBURGER.addEventListener('click', () => {
-    NAVIGATION.classList.toggle('header__wrapper__nav-open');
-    HAMBURGER.classList.toggle('header__wrapper-hamburger-active');
-    BODY.classList.toggle('active');
-})
+    const SCROLL_UP = document.getElementById('up');
+
+    HAMBURGER.addEventListener('click', () => {
+        NAVIGATION.classList.toggle('header__wrapper__nav-open');
+        HAMBURGER.classList.toggle('header__wrapper-hamburger-active');
+        BODY.classList.toggle('active');
+    })
+
+    window.addEventListener('scroll', () => {
+        if (window.innerWidth <= 768 && window.scrollY > 300) {
+            SCROLL_UP.classList.add('show');
+            console.log('h');
+        } else {
+            SCROLL_UP.classList.remove('show');
+        }
+    });
+
+    SCROLL_UP.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+
+});
