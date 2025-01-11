d89c16db79d6220e0672d870c1a2b68069521b37
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-01-11 16:15:41 +0300

    fix: solve error while enable buttons

--- simon-says/pages/main/js/script.js
+++ simon-says/pages/main/js/script.js
@@ -21,7 +21,7 @@ let isElementsLocked = false;
 function lockElement(element, lock = true) {
   isElementsLocked = lock;
   lock ? element.classList.add('locked') : element.classList.remove('locked');
-  lock ? element.disabled : element.enable();
+  lock ? element.disabled : element.enable;
 }
 
 /*
