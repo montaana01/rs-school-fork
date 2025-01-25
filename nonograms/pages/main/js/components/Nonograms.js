1fba98e86e6ce2eaaf9e9ac420a51cb99e5dcce9
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-01-25 13:32:56 +0300

    fix: use Header class

--- nonograms/pages/main/js/components/Nonograms.js
+++ nonograms/pages/main/js/components/Nonograms.js
@@ -1,3 +1,5 @@
+import { Header } from "./html/Header.js";
+
 export class Nonograms {
   constructor(difficult) {
     this.difficult = difficult;
@@ -5,16 +7,15 @@ export class Nonograms {
 
   start() {
     //here some code for starting game
-    console.log(`Starting nonograms with difficult "${this.difficult}"`);
+    const header = new Header();
+    document.body.appendChild(header.getElement());
   }
 
   pause() {
     //here code for pause
-    console.log("Paused nonograms");
   }
 
   finish() {
     //here call to finish screen with stats
-    console.log("Finished nonograms");
   }
 }
