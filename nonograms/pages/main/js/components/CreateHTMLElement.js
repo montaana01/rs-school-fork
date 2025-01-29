8120c2923692aafa1411009db3311694c2893667
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-01-29 20:11:04 +0300

    feat: create method in CreateHTMLElement.js that toggle class and fix name of variable in updateClass()

--- nonograms/pages/main/js/components/CreateHTMLElement.js
+++ nonograms/pages/main/js/components/CreateHTMLElement.js
@@ -31,7 +31,11 @@ export class CreateHTMLElement {
     this.element.textContent = newContent;
   }
 
-  updateClass(ClassName) {
-    this.element.className += ` ${ClassName}`;
+  updateClass(className) {
+    this.element.className += ` ${className}`;
+  }
+
+  toggleClass(className) {
+    this.element.classList.toggle(className);
   }
 }
