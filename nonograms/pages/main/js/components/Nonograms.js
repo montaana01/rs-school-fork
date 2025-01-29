c3335b1b7dffc95a36051eaf29fe5382949d91f4
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-01-29 23:30:11 +0300

    feat: create test solution and implement Game class in Nonograms class

--- nonograms/pages/main/js/components/Nonograms.js
+++ nonograms/pages/main/js/components/Nonograms.js
@@ -1,5 +1,6 @@
 import { Header } from "./html/Header.js";
 import { Footer } from "./html/Footer.js";
+import { Game } from "./html/Game.js";
 import { Theme } from "./Theme.js";
 
 export class Nonograms {
@@ -12,7 +13,26 @@ export class Nonograms {
     const SCRIPT = document.body.querySelector("script");
     const header = new Header();
     const footer = new Footer();
+
+    //todo create global states class with templates of game!
+    const solution = {
+      length: 10,
+      array: [
+        [0, 0, 1, 0, 0, 0, 0, 1, 0, 0], // □ □ ■ □ □ □ □ ■ □ □
+        [0, 1, 0, 1, 0, 0, 1, 0, 1, 0], // □ ■ □ ■ □ □ ■ □ ■ □
+        [1, 1, 0, 1, 1, 1, 1, 0, 1, 1], // ■ ■ □ ■ ■ ■ ■ □ ■ ■
+        [0, 1, 1, 1, 0, 0, 1, 1, 1, 0], // □ ■ ■ ■ □ □ ■ ■ ■ □
+        [0, 0, 1, 1, 0, 0, 1, 1, 0, 0], // □ □ ■ ■ □ □ ■ ■ □ □
+        [0, 1, 0, 1, 1, 1, 1, 0, 1, 0], // □ ■ □ ■ ■ ■ ■ □ ■ □
+        [0, 1, 1, 1, 0, 0, 1, 1, 1, 0], // □ ■ ■ ■ □ □ ■ ■ ■ □
+        [1, 1, 1, 0, 1, 1, 0, 1, 1, 1], // ■ ■ ■ □ ■ ■ □ ■ ■ ■
+        [0, 1, 1, 1, 0, 0, 1, 1, 1, 0], // □ ■ ■ ■ □ □ ■ ■ ■ □
+        [0, 1, 0, 0, 1, 1, 0, 0, 1, 0], // □ ■ □ □ ■ ■ □ □ ■ □
+      ],
+    };
+    const GAME = new Game(solution.length, solution.array);
     SCRIPT.before(header.getElement());
+    SCRIPT.before(GAME.render());
     SCRIPT.before(footer.getElement());
     const THEME = new Theme(header.getThemeSwitcher());
     THEME.init();
