11699505097ce26f3ccf8b94ea1aa1398108b872
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-01-31 00:02:35 +0300

    fix: update Nonograms.js -- remove solution array edit constructor for using array of solutions and get infos from them

--- nonograms/pages/main/js/components/Nonograms.js
+++ nonograms/pages/main/js/components/Nonograms.js
@@ -4,35 +4,24 @@ import { Game } from "./html/Game.js";
 import { Theme } from "./Theme.js";
 
 export class Nonograms {
-  constructor(difficult) {
-    this.difficult = difficult;
+  constructor(solutions) {
+    this.solutions = solutions;
+    this.currentGame = null;
   }
 
-  start() {
+  start(difficult, levelName) {
+    const template = this.solutions[difficult].templates.find(
+      (template) => template.name === levelName
+    );
+    this.currentGame = new Game(this.solutions[difficult].size, template.data);
+
     //here some code for starting game
     const SCRIPT = document.body.querySelector("script");
     const header = new Header();
     const footer = new Footer();
 
-    //todo create global states class with templates of game!
-    const solution = {
-      length: 10,
-      array: [
-        [0, 0, 1, 0, 0, 0, 0, 1, 0, 0], // □ □ ■ □ □ □ □ ■ □ □
-        [0, 1, 0, 1, 0, 0, 1, 0, 1, 0], // □ ■ □ ■ □ □ ■ □ ■ □
-        [1, 1, 0, 1, 1, 1, 1, 0, 1, 1], // ■ ■ □ ■ ■ ■ ■ □ ■ ■
-        [0, 1, 1, 1, 0, 0, 1, 1, 1, 0], // □ ■ ■ ■ □ □ ■ ■ ■ □
-        [0, 0, 1, 1, 0, 0, 1, 1, 0, 0], // □ □ ■ ■ □ □ ■ ■ □ □
-        [0, 1, 0, 1, 1, 1, 1, 0, 1, 0], // □ ■ □ ■ ■ ■ ■ □ ■ □
-        [0, 1, 1, 1, 0, 0, 1, 1, 1, 0], // □ ■ ■ ■ □ □ ■ ■ ■ □
-        [1, 1, 1, 0, 1, 1, 0, 1, 1, 1], // ■ ■ ■ □ ■ ■ □ ■ ■ ■
-        [0, 1, 1, 1, 0, 0, 1, 1, 1, 0], // □ ■ ■ ■ □ □ ■ ■ ■ □
-        [0, 1, 0, 0, 1, 1, 0, 0, 1, 0], // □ ■ □ □ ■ ■ □ □ ■ □
-      ],
-    };
-    const GAME = new Game(solution.length, solution.array);
     SCRIPT.before(header.getElement());
-    SCRIPT.before(GAME.render());
+    SCRIPT.before(this.currentGame.render());
     SCRIPT.before(footer.getElement());
     const THEME = new Theme(header.getThemeSwitcher());
     THEME.init();
