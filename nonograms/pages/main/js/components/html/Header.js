258615ac2a6d3fb56d9bce7995f65c4d956d4638
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-02-02 02:50:59 +0300

    feat: implement difficult switcher in header

--- nonograms/pages/main/js/components/html/Header.js
+++ nonograms/pages/main/js/components/html/Header.js
@@ -1,4 +1,5 @@
 import { CreateHTMLElement } from "./../CreateHTMLElement.js";
+import { hardness } from "../Global.js";
 
 export class Header {
   constructor(options) {
@@ -27,39 +28,86 @@ export class Header {
     LOGO_IMG.appendChildTo(LOGO.element);
     LOGO.appendChildTo(HEADER_WRAPPER.element);
 
-    const RATING = new CreateHTMLElement("div", {
+    // todo:
+    // if game started we introduce timer numbers
+    //
+    // const RATING = new CreateHTMLElement("div", {
+    //   className: "header__wrapper-item",
+    // });
+    // const RATING_VALUE = new CreateHTMLElement("p", {
+    //   id: "header__wrapper__rating-value",
+    //   content: "0",
+    // });
+    // RATING_VALUE.appendChildTo(RATING.element);
+    // RATING.appendChildTo(HEADER_WRAPPER.element);
+
+    const DIFFICULTY_SWITCHER = new CreateHTMLElement("div", {
       className: "header__wrapper-item",
     });
-    const RATING_VALUE = new CreateHTMLElement("p", {
-      id: "header__wrapper__rating-value",
-      content: "0",
-    });
-    RATING_VALUE.appendChildTo(RATING.element);
-    RATING.appendChildTo(HEADER_WRAPPER.element);
+    DIFFICULTY_SWITCHER.updateClass("header__wrapper-item__switcher");
 
-    const DIFFICULT = new CreateHTMLElement("div", {
-      className: "header__wrapper-item",
+    const SWITCHER_AREA = new CreateHTMLElement("div", {
+      className: "header__wrapper-item__switcher__area",
     });
-    const LEVELS = ["easy (5х5)", "medium (10х10)", "hard (15х15)"];
 
-    LEVELS.forEach((difficulty) => {
-      const button = new CreateHTMLElement("button", {
-        className: "header__wrapper__difficulty-selector",
-        attributes: { "data-difficulty": difficulty },
-        content: difficulty.charAt(0).toUpperCase() + difficulty.slice(1),
+    const SWITCHER_POINT = new CreateHTMLElement("div", {
+      className: "header__wrapper-item__switcher__point",
+    });
+
+    const SWITCHER_TEXT = new CreateHTMLElement("div", {
+      className: "header__wrapper-item__switcher__text",
+    });
+
+    hardness.forEach((level, index) => {
+      const marker = new CreateHTMLElement("div", {
+        className: "header__wrapper-item__switcher__point-marker",
+        attributes: {
+          "data-position": index,
+          "data-difficulty": level,
+        },
       });
-      button.appendChildTo(DIFFICULT.element);
+      marker.updateClass(`${index === 0 ? "active" : ""}`);
+
+      const label = new CreateHTMLElement("p", {
+        className: "header__wrapper-item__switcher__text-label",
+        content: level.charAt(0).toUpperCase() + level.slice(1),
+      });
+      label.updateClass(` ${index === 0 ? "active" : ""}`);
+
+      marker.element.addEventListener("click", () => {
+        localStorage.setItem("difficulty", level);
+
+        SWITCHER_POINT.element
+          .querySelectorAll(".header__wrapper-item__switcher__point-marker")
+          .forEach((marker) => marker.classList.remove("active"));
+
+        SWITCHER_TEXT.element
+          .querySelectorAll(".header__wrapper-item__switcher__text-label")
+          .forEach((text) => text.classList.remove("active"));
+
+        marker.element.classList.add("active");
+        label.element.classList.add("active");
+      });
+
+      marker.appendChildTo(SWITCHER_POINT.element);
+      label.appendChildTo(SWITCHER_TEXT.element);
     });
 
-    DIFFICULT.appendChildTo(HEADER_WRAPPER.element);
+    SWITCHER_POINT.appendChildTo(SWITCHER_AREA.element);
+    SWITCHER_AREA.appendChildTo(DIFFICULTY_SWITCHER.element);
+
+    SWITCHER_TEXT.appendChildTo(DIFFICULTY_SWITCHER.element);
+    DIFFICULTY_SWITCHER.appendChildTo(HEADER_WRAPPER.element);
 
     this.THEME = new CreateHTMLElement("button", {
-      className: "header__wrapper__difficulty-selector",
+      className: "header__wrapper-item",
       content: "Theme",
       attributes: {
         id: "theme-switcher",
       },
     });
+    this.THEME.updateClass("header__wrapper-item__theme");
+    this.THEME.updateClass("btn");
     this.THEME.appendChildTo(HEADER_WRAPPER.element);
   }
 
