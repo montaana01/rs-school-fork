e94af9c93e67f708e26f5087ca9251a9145c5866
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-02-03 01:04:17 +0300

    feat: implement timer

--- nonograms/pages/main/js/components/html/Game.js
+++ nonograms/pages/main/js/components/html/Game.js
@@ -9,6 +9,9 @@ export class Game {
     this.answersArray = [];
     this.answersCount = 0;
     this.cellsArray = [];
+    this.isTimerRunning = false;
+    this.startTime = null;
+
     this.MAIN = new CreateHTMLElement("main");
     this.GRID = new CreateHTMLElement("section", { className: "game" });
     this.GRID.appendChildTo(this.MAIN.element);
@@ -32,6 +35,10 @@ export class Game {
       className: "game__wrapper__info",
     });
     this.INFO.updateClass("border-right border-bottom");
+
+    this.timer = new CreateHTMLElement("div", {
+      className: "game__wrapper__info-timer",
+    });
 
     this.ANSWERS_COLUMN = new CreateHTMLElement("div", {
       className: "game__wrapper__answers",
@@ -59,6 +66,7 @@ export class Game {
       this.renderAnswersRow();
     }
     this.renderField();
+    this.timer.appendChildTo(this.INFO.element);
     return this.MAIN.element;
   }
 
@@ -152,12 +160,14 @@ export class Game {
         cell.element.addEventListener("mousedown", (e) => {
           if (e.button === 2) {
             e.preventDefault();
+            if (!this.isTimerRunning) this.startTimer();
             cell.element.classList.remove("black");
             cell.toggleClass("crossed");
             this.checkSolution();
           }
         });
         cell.element.addEventListener("click", () => {
+          if (!this.isTimerRunning) this.startTimer();
           cell.element.classList.remove("crossed");
           cell.toggleClass("black");
           this.checkSolution();
@@ -223,8 +233,26 @@ export class Game {
       //todo add sound effect to advanced level
       this.showFinalMessage();
     }
+  }
 
-    return isCorrect;
+  startTimer() {
+    this.startTime = Date.now();
+    this.isTimerRunning = true;
+    this.timerInterval = setInterval(() => {
+      this.updateTimer();
+    }, 1000);
+  }
+
+  updateTimer() {
+    const elapsed = Math.floor((Date.now() - this.startTime) / 1000);
+    this.timer.updateContent(
+      `${Math.floor(elapsed / 60)}:${(elapsed % 60).toString().padStart(2, "0")}`
+    );
+  }
+
+  stopTimer() {
+    clearInterval(this.timerInterval);
+    this.isTimerRunning = false;
   }
 
   showCarousel(solutions) {
@@ -240,10 +268,17 @@ export class Game {
   }
 
   showFinalMessage() {
+    const solveTime = Math.floor((Date.now() - this.startTime) / 1000);
+    this.stopTimer();
+    const minutes = Math.floor(solveTime / 60);
+    const seconds = solveTime % 60;
+    const solveTimeString = `${minutes}:${seconds.toString().padStart(2, "0")}`;
+
     const finalPopUp = new Popup(
       "popup",
-      "Great! You have solved the nonogram!"
+      `Great! You solved it in ${solveTimeString}!`
     );
+    localStorage.setItem("solveTime", solveTimeString);
     finalPopUp.getPopup().appendChildTo(this.MAIN.element);
   }
 }
