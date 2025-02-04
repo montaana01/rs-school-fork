c1073f4897b0c97f85d71c672398eb5d86df3022
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-02-04 21:06:18 +0300

    feat: implement sound logic! and some edits

---
+++ nonograms/pages/main/js/components/Sound.js
@@ -0,0 +1,34 @@
+export class Sound {
+  constructor() {
+    this.sounds = {
+      click: new Audio("./../../assets/sound/click.wav"),
+      remove: new Audio("./../../assets/sound/remove.mp3"),
+      cross: new Audio("./../../assets/sound/cross.mp3"),
+      win: new Audio("./../../assets/sound/win.wav"),
+    };
+
+    this.enabled = localStorage.getItem("sound") !== "false";
+    this.init();
+  }
+
+  init() {
+    this.BUTTON = document.getElementById("sound");
+    if (this.BUTTON) {
+      this.BUTTON.textContent = `${this.enabled ? "🔊" : "🔇"}`;
+    }
+  }
+
+  play(sound) {
+    if (this.enabled && this.sounds[sound]) {
+      this.sounds[sound].currentTime = 0;
+      this.sounds[sound].volume = 0.11;
+      this.sounds[sound].play();
+    }
+  }
+
+  toggleSound() {
+    this.enabled = !this.enabled;
+    localStorage.setItem("sound", this.enabled);
+    this.init();
+  }
+}
