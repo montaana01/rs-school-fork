e301a25230834bd9a62f41324c0ffbb39c6f464b
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-03-16 23:11:16 +0300

    fix: remove bad innerHTML method inside main, add method of main view

--- decision-making-tool/src/app/app.ts
+++ decision-making-tool/src/app/app.ts
@@ -1,35 +1,31 @@
 import './../style.scss';
 import './view/assets.scss';
 import './view/main/main.scss';
-import FooterView from './view/footer/footerView.ts';
 import HeaderView from './view/header/headerView.ts';
-import decisionMakingToolLogo from '/icons/decision-making-tool.png';
+import MainView from './view/main/main.ts';
+import FooterView from './view/footer/footerView.ts';
 
 export default class App {
   private BODY: HTMLElement;
   private headerView: HeaderView;
-  private mainView: HTMLElement;
+  private mainView: HeaderView;
   private footerView: FooterView;
 
   constructor() {
     this.BODY = document.body;
     this.headerView = new HeaderView();
-    this.mainView = document.createElement('main');
+    this.mainView = new MainView();
     this.footerView = new FooterView();
     this.createView();
   }
 
   private createView(): void {
-    this.BODY.append(this.headerView.getHTMLElement(), this.mainView, this.footerView.getHTMLElement());
-    document.querySelector<HTMLDivElement>('main')!.innerHTML = `
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src="${decisionMakingToolLogo}" class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://www.typescriptlang.org/" target="_blank">
-          <img src="${decisionMakingToolLogo}" class="logo vanilla" alt="TypeScript logo" />
-        </a>
-      </div>
-    `;
+    //todo: use theme switcher class for remove hardcode
+    this.BODY.classList.add('light');
+    this.BODY.append(
+      this.headerView.getHTMLElement(),
+      this.mainView.getHTMLElement(),
+      this.footerView.getHTMLElement(),
+    );
   }
 }
