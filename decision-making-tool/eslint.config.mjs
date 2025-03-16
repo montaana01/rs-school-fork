64a5738a0420fff39a18a83928616630355e501e
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-03-16 22:09:55 +0300

    feat: update config with adding new rule to block alerts

--- decision-making-tool/eslint.config.mjs
+++ decision-making-tool/eslint.config.mjs
@@ -22,6 +22,7 @@ export default [
       reportUnusedDisableDirectives: 'error',
     },
     rules: {
+      'no-alert': 'error',
       'max-lines-per-function': ['error', 40],
       'no-console': 'warn',
       '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
