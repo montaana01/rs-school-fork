8c18aba004344df869fb5928f13f819e9c126c83
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-03-16 22:22:45 +0300

    fix: create final version of footer markup

--- decision-making-tool/src/app/view/footer/footerView.ts
+++ decision-making-tool/src/app/view/footer/footerView.ts
@@ -1,15 +1,79 @@
 import './footerView.scss';
 import type { SettingsType } from '../../types/SettingsType.ts';
 import View from '../view.ts';
+import HtmlElementCreator from '../../services/htmlElementCreator.ts';
+import { HtmlImgElementCreator } from '../../services/htmlImgElementCreator.ts';
 
 export default class FooterView extends View {
+  private readonly githubSettings: SettingsType;
+  private readonly copyrightSettings: SettingsType;
+  private readonly rssSettings: SettingsType;
+
   constructor() {
     const footerSettings: SettingsType = {
       tagName: 'footer',
       classNames: ['footer'],
-      textContent: 'The Rolling Scopes School',
       callback: null,
     };
     super(footerSettings);
+
+    this.githubSettings = {
+      tagName: 'img',
+      classNames: ['footer__wrapper-item', 'link'],
+      callback: (): Window | null => window.open('https://github.com/montaana01', '_blank'),
+      src: './icons/github.svg',
+      alt: 'Github icon',
+    };
+
+    this.copyrightSettings = {
+      tagName: 'p',
+      classNames: ['footer__wrapper-item', 'link'],
+      textContent: `YakovlevDev © ${new Date().getFullYear()}`,
+      callback: (): Window | null => window.open('https://github.com/montaana01', '_blank'),
+    };
+
+    this.rssSettings = {
+      tagName: 'img',
+      classNames: ['footer__wrapper-item', 'link'],
+      callback: (): Window | null => window.open('https://rs.school', '_blank'),
+      src: './icons/rss-logo.svg',
+      alt: 'Made in Rolling Scopes School',
+    };
+    this.configureView();
+  }
+
+  public configureView(): void {
+    const containerSettings: SettingsType = {
+      tagName: 'div',
+      classNames: ['container'],
+    };
+    const container = new HtmlElementCreator(containerSettings);
+    this.elementCreator.addInnerHtmlCreatorElement(container);
+
+    const wrapperSettings: SettingsType = {
+      tagName: 'div',
+      classNames: ['footer', 'footer__wrapper'],
+    };
+
+    const wrapper = new HtmlElementCreator(wrapperSettings);
+    container.addInnerHtmlCreatorElement(wrapper);
+
+    const github = this.getFooterGithubElement();
+    wrapper.addInnerHtmlCreatorElement(github);
+    const copyright = this.getFooterCopyrightElement();
+    wrapper.addInnerHtmlCreatorElement(copyright);
+    const rss = this.getFooterRssElement();
+    wrapper.addInnerHtmlCreatorElement(rss);
+  }
+
+  public getFooterGithubElement(): HtmlElementCreator {
+    return new HtmlImgElementCreator(this.githubSettings);
+  }
+
+  private getFooterCopyrightElement(): HtmlElementCreator {
+    return new HtmlElementCreator(this.copyrightSettings);
+  }
+  private getFooterRssElement(): HtmlElementCreator {
+    return new HtmlImgElementCreator(this.rssSettings);
   }
 }
