402313b394995fb79f94171ae0583482bf1aa62c
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-03-18 01:31:51 +0300

    feat: implement theme switcher class

---
+++ decision-making-tool/src/app/view/theme.ts
@@ -0,0 +1,34 @@
+import StorageManager from './../services/storageManager';
+
+enum ThemeEnum {
+  Dark = 'dark',
+  Light = 'light',
+}
+
+export default class Theme {
+  private storageManager: StorageManager;
+  private readonly THEME_KEY: string;
+
+  constructor() {
+    this.THEME_KEY = 'theme';
+    this.storageManager = StorageManager.getInstance();
+
+    const savedTheme: string | null = this.storageManager.load<string>(this.THEME_KEY);
+    const systemPrefersLight: boolean = window.matchMedia('(prefers-color-scheme: light)').matches;
+
+    const initialTheme: string = savedTheme || (systemPrefersLight ? ThemeEnum.Light : ThemeEnum.Dark);
+    this.applyTheme(initialTheme);
+  }
+
+  public toggleTheme(): void {
+    const currentTheme: string = document.body.classList.contains(ThemeEnum.Dark) ? ThemeEnum.Dark : ThemeEnum.Light;
+    const newTheme: string = currentTheme === ThemeEnum.Dark ? ThemeEnum.Light : ThemeEnum.Dark;
+    this.applyTheme(newTheme);
+  }
+
+  private applyTheme(theme: string): void {
+    document.body.classList.remove(ThemeEnum.Light, ThemeEnum.Dark);
+    document.body.classList.add(theme);
+    this.storageManager.save(this.THEME_KEY, theme);
+  }
+}
