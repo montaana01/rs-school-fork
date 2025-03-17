4e727ba9b4d95c2146f10b1bb56678795bcf9707
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-03-17 03:38:19 +0300

    feat: create router class to moves from one page to other

---
+++ decision-making-tool/src/app/router.ts
@@ -0,0 +1,77 @@
+import type View from './view/view';
+import type { OptionsListItemsType } from './types/OptionsListItemsType.ts';
+
+import ListMainStateView from './view/main/main-states/list-items/listMainState';
+import DecisionMainStateView from './view/main/main-states/decisionMainState';
+import ErrorMainStateView from './view/main/main-states/errorMainState.ts';
+import ModalWindow from './view/main/modal/modalView.ts';
+
+export default class Router {
+  private container: HTMLElement;
+  private readonly routes: Record<string, () => View>;
+  private dataString: string | null;
+  private optionsListItems: OptionsListItemsType[] | undefined;
+
+  constructor(container: HTMLElement) {
+    this.container = container;
+
+    this.routes = {
+      '#': (): ListMainStateView => new ListMainStateView(),
+      '#list': (): ListMainStateView => new ListMainStateView(),
+      '#decision': (): DecisionMainStateView => new DecisionMainStateView(),
+    };
+    this.dataString = localStorage.getItem('options');
+    window.addEventListener('hashchange', this.handleRouteChange.bind(this));
+    window.addEventListener('load', this.handleRouteChange.bind(this));
+  }
+
+  //todo: update this method - maybe move this to new class that's interact with Json
+  private static parseOptionsList(json: string): OptionsListItemsType[] {
+    try {
+      const parsed = JSON.parse(json);
+      if (!Array.isArray(parsed)) {
+        return [];
+      }
+
+      return parsed.map((item) => {
+        const title = typeof item.title === 'string' ? item.title : '';
+        const weight = typeof item.weight === 'number' ? item.weight : 0;
+        return { title, weight };
+      });
+    } catch (error) {
+      return [];
+    }
+  }
+
+  private handleRouteChange(): void {
+    let routeFactory = this.routes[window.location.hash];
+    if (!routeFactory) {
+      routeFactory = (): ErrorMainStateView => new ErrorMainStateView();
+    }
+
+    if (window.location.hash === '#decision' && !this.canWeMoveToDecisionWheel()) {
+      ModalWindow.show('Add at least 2 valid options before choosing a solution!');
+      window.location.hash = '#list';
+      return;
+    }
+
+    while (this.container.firstChild) {
+      this.container.removeChild(this.container.firstChild);
+    }
+
+    const view: View = routeFactory();
+    this.container.appendChild(view.getHTMLElement());
+  }
+
+  private canWeMoveToDecisionWheel(): boolean {
+    this.dataString = localStorage.getItem('options');
+    if (!this.dataString) return false;
+
+    this.optionsListItems = Router.parseOptionsList(this.dataString);
+
+    const valid: OptionsListItemsType[] = this.optionsListItems.filter(
+      (option: OptionsListItemsType) => option.title.trim() !== '' && option.weight > 0,
+    );
+    return valid.length >= 2;
+  }
+}
