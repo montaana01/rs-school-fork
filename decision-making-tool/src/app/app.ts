85b9e5c64d5b5b7adbd2b5c654f8479498c05334
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-03-17 03:40:12 +0300

    fix: some updates and implement router in app

--- decision-making-tool/src/app/app.ts
+++ decision-making-tool/src/app/app.ts
@@ -1,22 +1,28 @@
 import './../style.scss';
 import './view/assets.scss';
 import './view/main/main.scss';
+
 import HeaderView from './view/header/headerView.ts';
-import MainView from './view/main/main.ts';
+import MainView from './view/main/mainView.ts';
 import FooterView from './view/footer/footerView.ts';
+import Router from './router';
 
 export default class App {
   private BODY: HTMLElement;
   private headerView: HeaderView;
-  private mainView: HeaderView;
+  private mainView: MainView;
   private footerView: FooterView;
 
   constructor() {
     this.BODY = document.body;
+
     this.headerView = new HeaderView();
     this.mainView = new MainView();
     this.footerView = new FooterView();
+
     this.createView();
+
+    new Router(this.mainView.getRouterContainer());
   }
 
   private createView(): void {
