273128603ca9bb6a844ee5163b14c19f85777254
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-03-17 03:41:36 +0300

    feat: create template for decision list main page view

---
+++ decision-making-tool/src/app/view/main/main-states/list-items/listMainState.ts
@@ -0,0 +1,37 @@
+import './list.scss';
+import View from './../../../view';
+import HtmlElementCreator from './../../../../services/htmlElementCreator';
+import type { SettingsType } from './../../../../types/SettingsType.ts';
+
+export default class ListMainStateView extends View {
+  constructor() {
+    const mainWrapperSettings: SettingsType = {
+      tagName: 'div',
+      classNames: ['main__wrapper'],
+    };
+
+    super(mainWrapperSettings);
+    this.configureView();
+  }
+
+  private configureView(): void {
+    // todo: replace this with table with decisions
+    const titleSettings = new HtmlElementCreator({
+      tagName: 'h2',
+      classNames: ['main__wrapper-item', 'main__wrapper-item__title'],
+      textContent: 'List of Options Page',
+    });
+    this.elementCreator.addInnerHtmlCreatorElement(titleSettings);
+
+    // todo: replace this with creating block of buttons
+    const goToDecisionButton: HtmlElementCreator = new HtmlElementCreator({
+      tagName: 'button',
+      classNames: ['main__wrapper-item', 'main__wrapper-item__button', 'button'],
+      textContent: 'Start',
+      callback: (): void => {
+        window.location.hash = 'decision';
+      },
+    });
+    this.elementCreator.addInnerHtmlCreatorElement(goToDecisionButton);
+  }
+}
