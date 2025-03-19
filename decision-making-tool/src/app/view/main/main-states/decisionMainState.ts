37b77fc00d68b1052c0765c3f492df3e32d51bd4
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-03-19 14:28:26 +0300

    fix: update decision main state class with adding html elements and some logic of them

--- decision-making-tool/src/app/view/main/main-states/decisionMainState.ts
+++ decision-making-tool/src/app/view/main/main-states/decisionMainState.ts
@@ -1,41 +1,140 @@
 import View from './../../view';
 import HtmlElementCreator from './../../../services/htmlElementCreator';
-import type { SettingsType } from '../../../types/SettingsType.ts';
+import { HtmlInputElementCreator } from '../../../services/htmlInputElementCreator';
+import type { SettingsType } from '../../../types/SettingsType';
+import ButtonView from './../../basicElements/buttonView';
 import StorageManager from '../../../services/storageManager';
+import ModalWindow from '../modal/modalView';
+import SoundManager from '../../../services/soundManager.ts';
+import type { OptionsListItemsType } from '../../../types/OptionsListItemsType';
 
 export default class DecisionMainStateView extends View {
-  private storageManager: StorageManager;
+  private storageManager: StorageManager = StorageManager.getInstance();
+  private soundManager: SoundManager = SoundManager.getInstance();
+  private soundButtonView: HTMLElement = HTMLElement;
+  private readonly durationSettings: SettingsType;
+  private readonly durationInputView: HtmlInputElementCreator;
+  private canvasElement: HTMLElement = HTMLElement;
+  private storageKey: string = 'options';
+  private duration: number = 0;
+  private decisionState: string;
 
   constructor() {
     const mainWrapperSettings: SettingsType = {
       tagName: 'div',
       classNames: ['main__wrapper'],
     };
-
     super(mainWrapperSettings);
+
+    this.storageManager.save('page', 'decision');
+    this.durationSettings = {
+      tagName: 'input',
+      classNames: ['main__wrapper-item__controls-input'],
+      placeholder: 'Duration (sec)',
+      type: 'number',
+    };
+    this.durationInputView = new HtmlInputElementCreator(this.durationSettings);
+
+    this.decisionState = 'initial';
+    this.storageManager.save('pageState', this.decisionState);
+
     this.configureView();
-    this.storageManager = StorageManager.getInstance();
   }
 
   private configureView(): void {
-    // todo: replace this with decision round picked
-    const errorTitleSettings: SettingsType = {
+    const rootElement: HTMLElement = this.elementCreator.getCreatedElement();
+    while (rootElement.firstChild) {
+      rootElement.removeChild(rootElement.firstChild);
+    }
+
+    const titleSettings: SettingsType = {
       tagName: 'h2',
-      classNames: ['main__wrapper-item', 'main__wrapper-item__title'],
+      classNames: ['main__wrapper-item__subtitle'],
       textContent: 'Decision Picker Page',
     };
-    this.storageManager.save('page', 'decision');
-    this.elementCreator.addInnerHtmlCreatorElement(new HtmlElementCreator(errorTitleSettings));
+    this.elementCreator.addInnerHtmlCreatorElement(new HtmlElementCreator(titleSettings));
 
-    const backToListButton: SettingsType = {
-      tagName: 'button',
-      classNames: ['main__wrapper-item', 'main__wrapper-item__button', 'button'],
-      textContent: 'Go to decision list page',
-      callback: (): void => {
+    this.elementCreator.addInnerHtmlCreatorElement(this.getControlPanelView());
+
+    this.elementCreator.addInnerHtmlCreatorElement(this.getCanvasElementView());
+  }
+
+  private getControlPanelView(): HtmlElementCreator {
+    const controlPanelSettings: SettingsType = {
+      tagName: 'div',
+      classNames: ['main__wrapper-item', 'main__wrapper-item__controls'],
+    };
+    const controlPanel: HtmlElementCreator = new HtmlElementCreator(controlPanelSettings);
+
+    const backButtonView: ButtonView = new ButtonView(
+      'Back',
+      (): void => {
         window.location.hash = '#/list';
       },
-    };
+      ['main__wrapper-item__controls-back'],
+    );
+    controlPanel.addInnerHtmlElement(backButtonView.getHTMLElement());
 
-    this.elementCreator.addInnerHtmlCreatorElement(new HtmlElementCreator(backToListButton));
+    const soundButtonView: ButtonView = new ButtonView('🔊', () => {}, ['main__wrapper-item__controls-sound']);
+    this.soundButtonView = soundButtonView.getHTMLElement();
+    controlPanel.addInnerHtmlElement(this.soundButtonView);
+    this.soundManager.initializeSound(this.soundButtonView);
+
+    controlPanel.addInnerHtmlElement(this.durationInputView.getCreatedElement());
+
+    const pickButtonView: ButtonView = new ButtonView(
+      'Pick',
+      (): void => {
+        this.startPickingProcess();
+      },
+      ['main__wrapper-item__controls-pick'],
+    );
+    controlPanel.addInnerHtmlElement(pickButtonView.getHTMLElement());
+
+    const pickedOptionSettings: SettingsType = {
+      tagName: 'div',
+      classNames: ['main__wrapper-item__controls-subtitle'],
+      textContent: 'Please set up the picking process.',
+    };
+    controlPanel.addInnerHtmlCreatorElement(new HtmlElementCreator(pickedOptionSettings));
+    return controlPanel;
+  }
+
+  private startPickingProcess(): void {
+    this.decisionState = 'picking';
+    this.storageManager.save('pageState', this.decisionState);
+    // todo: get duration from input
+
+    // todo: write disable and enable controls function
+    //this.disableControls();
+
+    this.duration = Number(this.durationInputView.element.value);
+    if (!this.duration || this.duration < 5) new ModalWindow('Please enter duration >5 secs!');
+    // todo: write choosing process
+    const decisionData: OptionsListItemsType[] | null = this.storageManager.load(this.storageKey);
+    if (!Array.isArray(decisionData) || decisionData.length === 0) {
+      new ModalWindow("We don't have any information in localStorage");
+      return;
+    }
+    const dataText: string = decisionData
+      .map((item: OptionsListItemsType) => `ID: ${item.id}, Title: "${item.title}", Weight: ${item.weight}`)
+      .join('');
+
+    new ModalWindow(`This part of task is not realised! Duration ${this.duration} s 
+      And ${this.storageKey} = ${dataText}
+    `);
+  }
+
+  private getCanvasElementView(): HtmlElementCreator {
+    const canvasSettings: SettingsType = {
+      tagName: 'canvas',
+      classNames: ['main__wrapper-item', 'main__wrapper-item__canvas'],
+    };
+    const canvasElementView: HtmlElementCreator = new HtmlElementCreator(canvasSettings);
+
+    // todo: implement creating canvas element that consist of stored data from list of options view
+    this.canvasElement = canvasElementView.getCreatedElement();
+    this.canvasElement.setAttribute('height', '100');
+    return canvasElementView;
   }
 }
