a94c75bf6735d1e58bd6e004cb851d18eeca6b90
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-03-20 01:32:27 +0300

    feat: implement wheel section type

---
+++ decision-making-tool/src/app/types/WheelSectionsType.ts
@@ -0,0 +1,8 @@
+import type { OptionsListItemsType } from './OptionsListItemsType.ts';
+
+export type WheelSection = {
+  color: string;
+  startAngle: number;
+  endAngle: number;
+  option: OptionsListItemsType;
+};
