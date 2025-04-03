cc0052d763579709e870b3eec6c2113eb37c6484
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-04-03 12:58:59 +0300

    feat: image creator class implemented and write footer class

--- async-race/src/app/app.ts
+++ async-race/src/app/app.ts
@@ -3,13 +3,16 @@ import porsche from '/cars/porsche.svg';
 
 import './view/assets.scss';
 import ThemeManager from './services/themeManager';
+import FooterView from './view/footer/FooterView.ts';
 
 export default class App {
   private BODY: HTMLElement;
   private theme: ThemeManager;
+  private footerView: FooterView;
 
   constructor() {
     this.BODY = document.body;
+    this.footerView = new FooterView();
     this.theme = new ThemeManager();
   }
 
@@ -17,7 +20,7 @@ export default class App {
     this.BODY.innerHTML = `
     <div class="container">
         <a href="https://vite.dev" target="_blank">
-          <img src="${porsche}" class="logo" alt="Vite logo" />
+          <img src="${porsche}" class="logo" alt="Vite logo" width="50px"/>
         </a>
         <a href="https://rs.school/" target="_blank">
         </a>
@@ -31,10 +34,6 @@ export default class App {
         this.theme.toggleTheme();
       });
     }
-    // this.BODY.append(
-    //   this.headerView.getHTMLElement(),
-    //   this.mainView.getHTMLElement(),
-    //   this.footerView.getHTMLElement(),
-    // );
+    this.BODY.append(this.footerView.getFooter());
   }
 }
