3a1d16d4db44f8a8f3399762d88f3f2c5e415615
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-04-21 04:06:36 +0300

    fix: complete part of work and compile results of them on app.ts

--- fun-chat/src/app/app.ts
+++ fun-chat/src/app/app.ts
@@ -1,30 +1,83 @@
 import ThemeManager from './services/ThemeManager';
+import Router from './services/Router';
+
+import WebSocketManager from './services/websocket/WebSocketManager';
+import AuthService from './services/websocket/AuthService';
+
 import HeaderView from './view/header/HeaderView';
 import MainView from './view/main/MainView';
 import FooterView from './view/footer/FooterView';
+import ModalView from './view/modal/ModalView';
 
 const APP_NAME: string = 'Fun Chat!';
+
 export default class App {
   private readonly BODY: HTMLElement;
-  private headerView: HeaderView;
-  private mainView: MainView;
-  private footerView: FooterView;
-  private readonly theme: ThemeManager;
+  private headerView: HeaderView = new HeaderView(APP_NAME);
+  private mainView: MainView = new MainView();
+  private footerView: FooterView = new FooterView();
+
+  private theme: ThemeManager = new ThemeManager();
+  private webSocketManager!: WebSocketManager;
+  private auth!: AuthService;
+  private router!: Router;
 
   constructor() {
     this.BODY = document.body;
-    this.headerView = new HeaderView(APP_NAME);
-    this.mainView = new MainView();
-    this.footerView = new FooterView();
-    this.theme = new ThemeManager();
+
+    this.headerView.themeSwitcher.init(this.theme);
+
+    this.initWebSocket().then(async () => {
+      await this.auth.init();
+      this.router = new Router(this.mainView.getContainer(), this.auth)
+      this.initAuthListeners();
+      this.startApp();
+    });
   }
 
   public startApp(): void {
-    if (this.headerView.themeSwitcher) {
-      this.headerView.themeSwitcher.init(this.theme);
-    }
-    this.BODY.append(this.headerView.getHeader());
-    this.BODY.append(this.mainView.getMain());
-    this.BODY.append(this.footerView.getFooter());
+    this.BODY.append(this.headerView.getHeader(), this.mainView.getMain(), this.footerView.getFooter());
+
+    this.headerView.logoWrapper.setCallback(() => this.router.navigate('/'));
+    this.headerView.aboutButton.setCallback(() => this.router.navigate('/about'));
+
+    this.headerView.logoutButton.getCreatedElement().addEventListener('click', async () => {
+      try {
+        await this.auth.logout();
+        await this.router.navigate('/auth');
+      } catch (error) {
+        new ModalView(`Logout failed ${error}`, false);
+      }
+    });
+  }
+
+  private async initWebSocket(): Promise<void> {
+    this.webSocketManager = new WebSocketManager('localhost', 4000, false, true, 5, 1000);
+    this.auth = new AuthService(this.webSocketManager);
+
+    return new Promise((resolve) => {
+      this.webSocketManager.onOpen = async (): Promise<void> => {
+        resolve();
+      };
+
+      this.webSocketManager.onError = (error): void => {
+        new ModalView(`WebSocket error: ${error}`, false);
+        resolve();
+      };
+
+      this.webSocketManager.connect();
+    });
+  }
+
+  private initAuthListeners(): void {
+    this.auth.subscribe(({ loggedIn, user }) => {
+      if (loggedIn && user) {
+        this.headerView.updateHeaderGreetings(user.login);
+        this.headerView.showLogout();
+      } else {
+        this.headerView.updateHeaderGreetings('Guest');
+        this.headerView.hideLogout();
+      }
+    });
   }
 }
